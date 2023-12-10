e2e7a3bb6aa1ffcead78a7a5619d1a4869be9389
Damian D'Souza <damian.dsouza@pw.edu.pl>
2023-12-10 23:37:07 +0100

    Basic Game class with specified number of players and deal method

---
+++ game.py
@@ -0,0 +1,36 @@
+from typing import Union
+from deck import Deck
+from players import HumanPlayer, ComputerPlayer
+
+
+class WrongPlayerNumber(Exception):
+    def __init__(
+        self,
+        player_number,
+        message="Number of players must be an int between 2 and 4",
+    ) -> None:
+        super().__init__(message, player_number)
+
+
+class Game:
+    def __init__(self, player_number: int) -> None:
+        if type(player_number) is not int or not 2 <= player_number <= 4:
+            raise WrongPlayerNumber(player_number)
+        self._players = [HumanPlayer()] + [ComputerPlayer() for i in range(player_number - 1)]
+        self._deck = Deck()
+        self.deal()
+
+    @property
+    def players(self) -> list[Union[HumanPlayer, ComputerPlayer]]:
+        return self._players
+
+    def deal(self):
+        for _ in range(5):
+            for player in self.players:
+                card = self._deck.deal()
+                player.add_card_to_deck(card)
+
+
+game = Game(3)
+for player in game._players:
+    print(player.player_info())
