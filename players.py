d66d441b748fdfd023f2d65b89d5f28238794779
Damian D'Souza <damian.dsouza@pw.edu.pl>
2023-12-12 03:30:00 +0100

    Added getters, setters and methods related to printing

--- players.py
+++ players.py
@@ -15,17 +15,36 @@ class HumanPlayer:
     def makao_status(self) -> bool:
         return self._makao_status
 
+    @makao_status.setter
+    def makao_status(self, status: bool) -> None:
+        self._makao_status = status
+
+    @property
+    def rank(self) -> Optional[int]:
+        return self._rank
+
+    @rank.setter
+    def rank(self, rank: int) -> None:
+        self._rank = rank
+
+    @property
+    def cards(self) -> list[Card]:
+        return self._cards
+
     def add_card_to_deck(self, card: Card) -> None:
         self._cards.append(card)
 
     def player_info(self, human_computer: str = "Human player") -> str:
-        cards: str = " ".join([str(card) for card in self._cards])
+        cards: str = " ".join([str(card) for card in self.cards])
         return (
             f"{human_computer}\n"
             f"  Deck: {cards}\n"
-            f"  Makao: {self._makao_status}\n"
-            f"  Rank: {self._rank}"
+            f"  Makao: {self.makao_status}\n"
+            f"  Rank: {self.rank}"
         )
+
+    def __str__(self) -> str:
+        return " ".join([str(card) for card in self.cards])
 
 
 class ComputerPlayer(HumanPlayer):
@@ -37,3 +56,9 @@ class ComputerPlayer(HumanPlayer):
 
     def player_info(self, human_computer: str = "Computer player") -> str:
         return super().player_info(human_computer)
+
+    def _print_visible_cards(self) -> str:
+        return " ".join([str(card) for card in self.cards])
+
+    def __str__(self) -> str:
+        return " ".join(["*" for card in self.cards])
