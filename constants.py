08776943deb3585850c90430e4e3196c5825a8f5
Damian D'Souza <damian.dsouza@pw.edu.pl>
2023-12-24 13:53:45 +0100

    Formatting

--- constants.py
+++ constants.py
@@ -1,2 +1,16 @@
 SYMBOLS: list[str] = ["clubs", "spades", "diamonds", "hearts"]
-VALUES: list[str] = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "jack", "queen", "king", "ace"]
+VALUES: list[str] = [
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "jack",
+    "queen",
+    "king",
+    "ace",
+]
