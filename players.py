2b79a4f6cea89c393a41c6123d9a58723a708231
Damian D'Souza <damian.dsouza@pw.edu.pl>
2024-01-01 17:18:47 +0100

    Added docstrings and modified draw_card method

--- players.py
+++ players.py
@@ -17,6 +17,7 @@ class HumanPlayer:
         self._rank: Optional[int] = None
         self._makao_status: bool = False
         self._played_card: bool = False
+        self._cards_played: int = 0
         self._drew_card: bool = False
         self._skip_turns: int = 0
         self._penalty: bool = False
@@ -36,14 +37,33 @@ class HumanPlayer:
 
     @property
     def drew_card(self) -> bool:
+        """
+        Returns True if player has played at least one car
+        """
         return self._drew_card
 
     @property
     def played_card(self) -> bool:
+        """
+        Returns True if player has played at least one card
+        """
         return self._played_card
 
     @property
+    def card_played(self) -> int:
+        """
+        Returns current amount of cards played in one turn
+        """
+        return self._cards_played
+
+    def reset_cards_played(self) -> None:
+        self._cards_played = 0
+
+    @property
     def penalty(self) -> bool:
+        """
+        Returns True if player is currently drawing penalty
+        """
         return self._penalty
 
     @penalty.setter
@@ -78,11 +98,11 @@ class HumanPlayer:
         return self._hand
 
     def draw_card(self, deck: Deck) -> None:
-        if not self.drew_card and not self.played_card:
+        if self.penalty:
+            self._hand.append(deck.deal())
+        elif not self.drew_card and not self.played_card:
             self._hand.append(deck.deal())
             self._drew_card = True
-        elif self.penalty:
-            self._hand.append(deck.deal())
 
     def play_card(self, card: Card) -> None:
         if (self.drew_card and card is self.hand[-1] and not self.played_card) or not self.drew_card:
@@ -126,11 +146,11 @@ class ComputerPlayer(HumanPlayer):
         center_card: Card = kwargs.get("center", None)
         players: list[Union[HumanPlayer, "ComputerPlayer"]] = kwargs.get("players", None)
         players.remove(self)
-        req_symbol: tuple = kwargs.get("symbol", None)
+        req_suit: tuple = kwargs.get("suit", None)
         req_value: tuple = kwargs.get("value", None)
         four_played: bool = kwargs.get("four", None)
         for card in self.hand:
-            if center_card and center_card.can_play(card):
+            if center_card and center_card.can_play(card, **kwargs):
                 if card.value in ("jack", "ace"):
                     return None
                 return card
